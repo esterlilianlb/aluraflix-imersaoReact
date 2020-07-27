be967c6cda361aeb7ec6cf4440ac8581bc891959
esterlilianlb <esterlilian2231@gmail.com>
2020-07-27 15:13:48 -0300

    header finalizado

--- aluraflix/src/components/Menu/index.js
+++ aluraflix/src/components/Menu/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Logo from '../../assets/Logo.png'
+import './Menu.css'
+import ButtonLink from '../ButtonLink';
 
 function Menu() {
   return (
-    <nav>
-      Aluraflix
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Myflix" />
+      </a>
+
+      <ButtonLink href="/" className="ButtonLink">
+        Novo vídeo
+      </ButtonLink>
+
     </nav>
   )
 }
