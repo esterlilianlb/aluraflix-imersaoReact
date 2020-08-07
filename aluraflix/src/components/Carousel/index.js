2de6318ba991c1623c4f4dae7a6d5288f878beec
esterlilianlb <esterlilian2231@gmail.com>
2020-08-07 12:22:57 -0300

    biblioteca slider funcionando

--- aluraflix/src/components/Carousel/index.js
+++ aluraflix/src/components/Carousel/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { VideoCardGroupContainer, Slider, Title, ExtraLink } from './styles';
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slider';
 
 function Carousel({
   ignoreFirstVideo,
