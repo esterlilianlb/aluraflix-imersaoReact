308a86ed3a6143d49e6b41c77e581b825040f804
esterlilianlb <esterlilian2231@gmail.com>
2020-08-07 11:59:21 -0300

    slider

---
+++ aluraflix/src/components/Carousel/components/Slider/index.js
@@ -0,0 +1,22 @@
+import SlickSlider from 'react-slick';
+import styled from 'styled-components';
+
+const Container = styled.ul`
+  padding: 0;
+  margin: 0;
+  .slick.prev,
+  .slick.next {
+    z-index: 50;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: 30px;
+    height: 30px;
+    transform: initial;
+    &:before {
+      font-size: 30px;
+    }
+  }
+`;
+
+export default Button;
--- aluraflix/src/index.js
+++ aluraflix/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -16,6 +17,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
