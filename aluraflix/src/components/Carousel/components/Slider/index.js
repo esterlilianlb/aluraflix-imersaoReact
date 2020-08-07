7a3f3c3137e7658d7d96ba99c850576fa8a39dbf
esterlilianlb <esterlilian2231@gmail.com>
2020-08-07 12:21:06 -0300

    bug do carrossel resolvido

--- aluraflix/src/components/Carousel/components/Slider/index.js
+++ aluraflix/src/components/Carousel/components/Slider/index.js
@@ -1,11 +1,12 @@
-import SlickSlider from 'react-slick';
+import React from 'react';
+import SlickSlider from 'react-slick'
 import styled from 'styled-components';
 
 const Container = styled.ul`
   padding: 0;
   margin: 0;
-  .slick.prev,
-  .slick.next {
+  .slick-prev,
+  .slick-next {
     z-index: 50;
     top: 0;
     bottom: 0;
@@ -17,6 +18,40 @@ const Container = styled.ul`
       font-size: 30px;
     }
   }
+  
+  .slick-prev {
+    left: 0;
+  }
+  .slick-next {
+    right: 16px;
+  }
 `;
 
-export default Button;
+export const SliderItem = styled.li`
+  margin-right: 16px;
+  img {
+    margin: 16px;
+    width: 298px;
+    height: 197px;
+    object-fit: cover;
+  }
+`;
+
+
+const Slider = ({ children }) => (
+  <Container>
+    <SlickSlider {...{
+      dots: false,
+      infinite: false,
+      speed: 300,
+      centerMode: false,
+      variableWidth: true,
+      adaptiveHeight: true,
+    }}
+    >
+      {children}
+    </SlickSlider>
+  </Container>
+);
+
+export default Slider;
